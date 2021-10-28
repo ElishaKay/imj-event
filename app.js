b99728f0888ca43352faf3710df3ce97583cc8c7
elishakramer <elisha.kramer@ericom.com>
2021-10-28 13:42:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+//general dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();
+var adminConfig = require('./config/adminconfig.js');
+
+
+//database configuration
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: adminConfig.mysql.host,
+    user: adminConfig.mysql.username,
+    password: adminConfig.mysql.password,
+    database: process.env.DATABASE
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+
+//route configuration
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+//internal app dependencies
+var api = require('./api/api.js')(app, connection);
+var routes = require('./routes/routes.js')(app, connection);
+var auth = require('./authentication/auth.js')(app, connection);
+
+//server initiation
+var port = adminConfig.port;
+app.listen(port, function() {
+    console.log('schemeBeam up and running on port ' + port);
+});
